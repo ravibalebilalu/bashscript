e84fb3f02f65a28d8145fe3be1b5e80e4771e87c
ravibalebilalu <81ravikiran@gmail.com>
2024-06-18 10:36:37 +0530

    for loop

---
+++ 14_for_loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#for loop
+#numericals
+for i in 1 2 3 4 5
+    do  
+        echo $i
+    done
+
+for i in {1..20}
+    do 
+        echo $i
+    done
+
+#strings
+
+for name in Krishna Radha Hayavadana Muruli Nandagopala
+    do 
+        echo $name
+    done
